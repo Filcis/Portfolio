f1ea4a16995d4ac1ce32654a6deee05a75a2173a
Filcis <cislakfilip@gmail.com>
2017-07-18 16:45:04 +0200

    added navigation logic and styling

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+// General js
+
+(function(){
+  window.onload = function() {
+    var elem = document.querySelector('.grid');
+    var msnry = new Masonry( elem, {
+      // options
+      itemSelector: '.grid-item',
+      percentPosition: true
+    });
+  };
+
+    var mainNavigation = document.getElementById('main_nav');
+    var submenuToggle = mainNavigation.querySelectorAll('.dropdown_toggle');
+
+    [].forEach.call(submenuToggle, function(toggle) {
+        toggle.addEventListener('click', function() {
+        var submenu = mainNavigation.querySelector('.dropdown_menu');
+        var arrow = mainNavigation.querySelector('.arrow_right');
+        submenu.classList.toggle('open');
+        arrow.classList.toggle('open');
+      });
+    });
+
+    var toggleMenuButton = document.getElementById('toggleMenuButton');
+    toggleMenuButton.addEventListener("click", function(e){
+      e.preventDefault();
+      var navbarCollapse = mainNavigation.querySelector('.navbar-collapse');
+      navbarCollapse.classList.toggle('collapse');
+    })
+
+})();
