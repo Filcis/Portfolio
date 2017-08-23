5b467456bd807c4ccbdd155e2b07679a362a8a34
Filcis <cislakfilip@gmail.com>
2017-08-23 14:17:30 +0200

    Tidied app.js by wrapping code into functions, added imagesLoaded plugin to avoid masonry stacking images on top of another

--- public/js/app.js
+++ public/js/app.js
@@ -2,46 +2,55 @@
 
 (function () {
 
-  var gridItems = document.querySelectorAll('.grid-item');
-
-  [].forEach.call(gridItems, function (item) {
-    if (item.clientWidth > item.clientHeight) {
-      item.className += ' size2';
+  function resizeGridItems() {
+    // Select all masonry grid items into NodeList gridItems
+    var gridItems = document.querySelectorAll('.grid-item');
+    // Iterate through all gridItems and add class size2 if they are horizontal
+    for (var i = 0; i < gridItems.length; i++) {
+      if (gridItems[i].clientWidth > gridItems[i].clientHeight) {
+        gridItems[i].className += ' size2';
+      }
     }
-  });
+  }
 
-  // MASONRY
-  window.onload = function () {
-    var grid = document.querySelector('.grid');
-
+  function initMasonry() {
     var msnry = new Masonry(grid, {
-      // options
       itemSelector: '.grid-item',
       percentPosition: true,
+      // basic grid column module
       columnWidth: '.grid-sizer'
-      // fitWidth: true,
-      // gutter: 50
     });
-  };
+  }
 
   //NAVIGATION
+  function initNav() {
+    var mainNavigation = document.getElementById('main_nav');
+    var submenuToggle = mainNavigation.querySelectorAll('.dropdown_toggle');
 
-  var mainNavigation = document.getElementById('main_nav');
-  var submenuToggle = mainNavigation.querySelectorAll('.dropdown_toggle');
-
-  [].forEach.call(submenuToggle, function (toggle) {
-    toggle.addEventListener('click', function () {
-      var submenu = mainNavigation.querySelector('.dropdown_menu');
-      var arrow = mainNavigation.querySelector('.arrow_right');
-      submenu.classList.toggle('open');
-      arrow.classList.toggle('open');
+    [].forEach.call(submenuToggle, function (toggle) {
+      toggle.addEventListener('click', function () {
+        var submenu = mainNavigation.querySelector('.dropdown_menu');
+        var arrow = mainNavigation.querySelector('.arrow_right');
+        submenu.classList.toggle('open');
+        arrow.classList.toggle('open');
+      });
     });
+
+    var toggleMenuButton = document.getElementById('toggleMenuButton');
+    toggleMenuButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      var navbarCollapse = mainNavigation.querySelector('.navbar-collapse');
+      navbarCollapse.classList.toggle('collapse');
+    });
+  }
+
+  var grid = document.querySelector('.grid');
+
+  initNav();
+  // initialise Masonry and image functions when images are loaded
+  imagesLoaded(grid, function () {
+    resizeGridItems();
+    initMasonry();
   });
 
-  var toggleMenuButton = document.getElementById('toggleMenuButton');
-  toggleMenuButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    var navbarCollapse = mainNavigation.querySelector('.navbar-collapse');
-    navbarCollapse.classList.toggle('collapse');
-  });
 })();
--- src/app.js
+++ src/app.js
@@ -1,46 +1,53 @@
 (function(){
 
-  let gridItems = document.querySelectorAll('.grid-item');
-
-  [].forEach.call(gridItems, (item) => {
-    if (item.clientWidth > item.clientHeight) {
-      item.className += ' size2';
+  function resizeGridItems () {
+    // Select all masonry grid items into NodeList gridItems
+    let gridItems = document.querySelectorAll('.grid-item');
+    // Iterate through all gridItems and add class size2 if they are horizontal
+    for (let i = 0; i< gridItems.length; i++) {
+      if (gridItems[i].clientWidth > gridItems[i].clientHeight) {
+        gridItems[i].className += ' size2';
+      }
     }
-  })
+  }
 
-  // MASONRY
-  window.onload = () => {
+  function initMasonry () {
     const grid = document.querySelector('.grid');
-
     const msnry = new Masonry( grid, {
-      // options
       itemSelector: '.grid-item',
       percentPosition: true,
+      // basic grid column module
       columnWidth: '.grid-sizer',
-      // fitWidth: true,
-      // gutter: 50
     });
-  };
+  }
 
   //NAVIGATION
+  function initNav() {
+      const mainNavigation = document.getElementById('main_nav');
+      const submenuToggle = mainNavigation.querySelectorAll('.dropdown_toggle');
 
-    const mainNavigation = document.getElementById('main_nav');
-    const submenuToggle = mainNavigation.querySelectorAll('.dropdown_toggle');
-
-    [].forEach.call(submenuToggle, (toggle) => {
-        toggle.addEventListener('click', function() {
-        const submenu = mainNavigation.querySelector('.dropdown_menu');
-        const arrow = mainNavigation.querySelector('.arrow_right');
-        submenu.classList.toggle('open');
-        arrow.classList.toggle('open');
+      [].forEach.call(submenuToggle, (toggle) => {
+          toggle.addEventListener('click', function() {
+          const submenu = mainNavigation.querySelector('.dropdown_menu');
+          const arrow = mainNavigation.querySelector('.arrow_right');
+          submenu.classList.toggle('open');
+          arrow.classList.toggle('open');
+        });
       });
-    });
 
-    const toggleMenuButton = document.getElementById('toggleMenuButton');
-    toggleMenuButton.addEventListener("click", function(e){
-      e.preventDefault();
-      const navbarCollapse = mainNavigation.querySelector('.navbar-collapse');
-      navbarCollapse.classList.toggle('collapse');
-    });
+      const toggleMenuButton = document.getElementById('toggleMenuButton');
+      toggleMenuButton.addEventListener("click", function(e){
+        e.preventDefault();
+        const navbarCollapse = mainNavigation.querySelector('.navbar-collapse');
+        navbarCollapse.classList.toggle('collapse');
+      });
+  }
+
+    initNav();
+
+imagesLoaded( grid, function() {
+  resizeGridItems();
+  initMasonry();
+});
 
 })();
