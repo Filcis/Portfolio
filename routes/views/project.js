27abcaaf3f7167552a6baa8367f511985c3832fe
Filcis <cislakfilip@gmail.com>
2017-09-04 11:56:29 +0200

    added similiar projects lists below each project page. Filtered by categories

--- routes/views/project.js
+++ routes/views/project.js
@@ -4,16 +4,14 @@ exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
+	locals.categoriesNames = [];
 
-	// locals.category = req.params.category;
-	locals.categories = 'brak kategorii';
   locals.data = {
     project: [],
 };
 
  // Load project
 view.on('init', function(next) {
-  console.log(req.params.projekt);
   var q = keystone.list('Project').model.findOne({key: req.params.projekt}).populate('categories');
   q.exec(function(err, results) {
 		if (err) {
@@ -21,11 +19,30 @@ view.on('init', function(next) {
 		}
 		else {
 			locals.project = results;
-			console.log(locals.project);
 		}
   next(err);
   });
 });
+
+//create category filter
+view.on('init', function (next) {
+	var categoriesNames  = locals.project.categories.map(function(el){
+		return el.name;
+	});
+	locals.categoriesNames = categoriesNames;
+	next();
+});
+
+// Load the posts
+	view.on('init', function (next) {
+		var q = keystone.list('Project').model.find({categories: { $in: locals.project.categories } })
+			.sort('-createdDate')
+			.populate('categories');
+			q.exec(function (err, results) {
+			locals.projects = results;
+			next(err);
+		});
+	});
 
 	// Render the view
 	view.render('project');
