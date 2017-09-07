e1d597d06dcf9aeb3af6487aece1d42a338204e6
Filcis <cislakfilip@gmail.com>
2017-09-07 09:30:19 +0200

    https cloudinary images

--- keystone.js
+++ keystone.js
@@ -19,13 +19,15 @@ keystone.init({
 	'views': 'templates/views',
 	'view engine': 'pug',
 	'mongo': process.env.MONGODB_ADDON_URI || "mongodb://localhost/fc_portfolio",
-	'port': process.env.PORT || 3000,
+	'port': process.env.PORT || '8080',
 	'emails': 'templates/emails',
-
+	'env' : process.env.NODE_ENV || "development",
 	'auto update': true,
 	'session': true,
 	'auth': true,
 	'user model': 'User',
+	//  ssl: "force",
+	// 'ssl port': process.env.SSLPORT || '0.0.0.0',
 });
 
 // Load your project's Models
@@ -43,6 +45,8 @@ keystone.set('locals', {
 
 keystone.set('cloudinary config', 'cloudinary://564966198917524:UtJlbJI_rt-TLWGATR96M8fNF1k@dool1dbrb' );
 
+// optional, will force cloudinary to serve images over https
+keystone.set('cloudinary secure', true);
 
 // Load your project's Routes
 keystone.set('routes', require('./routes'));
@@ -54,19 +58,6 @@ keystone.set('nav', {
 	enquiries: 'enquiries',
 	users: 'users',
 });
-
-// Start Keystone to connect to your database and initialise the web server
-
-
-if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
-	console.log('----------------------------------------'
-	+ '\nWARNING: MISSING MAILGUN CREDENTIALS'
-	+ '\n----------------------------------------'
-	+ '\nYou have opted into email sending but have not provided'
-	+ '\nmailgun credentials. Attempts to send will fail.'
-	+ '\n\nCreate a mailgun account and add the credentials to the .env file to'
-	+ '\nset up your mailgun integration');
-}
 
 
 keystone.start();
